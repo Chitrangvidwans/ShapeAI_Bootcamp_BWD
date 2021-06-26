0a14b80d094464826230bedca0317c7cbecf24b7
Chitrangvidwans <86519448+Chitrangvidwans@users.noreply.github.com>
2021-06-26 17:12:40 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>copyright by chitrang vidwans @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>react.js Bootcamp</h1>
+      <p>
+        A react.js Bootcamp lead by MR.Shaurya Sinha. It was an amazing
+        experience to be part of this Bootcamp. thank you ShapeAI for this
+        amazing Bootcamp.
+      </p>
+    </div>
+  );
+}
+export default Info;
